e2850a908e9fb8a87d296f49bb2680d3afdbfb42
Margarita R <margarita@arrayschool.com>
2017-12-08 14:56:53 -0700

    added host url from heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,10 +89,11 @@ Rails.application.configure do
   # Setup the mailer config
  config.action_mailer.delivery_method = :smtp
  config.action_mailer.perform_deliveries = true
+ config.action_mailer.default_url_options = { host:'powerful-cove-77996.herokuapp.com' }
  config.action_mailer.smtp_settings = {
    :user_name => ENV['SENDGRID_USERNAME'],
    :password => ENV['SENDGRID_PASSWORD'],
-   :domain => 'powerful-cove-77996.herokuapp.com/',
+   :domain => 'powerful-cove-77996.herokuapp.com',
    :address => 'smtp.sendgrid.net',
    :port => 587,
    :authentication => :plain,
