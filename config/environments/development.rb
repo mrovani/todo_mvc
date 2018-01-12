5cce540cddcab43d723c63812c7571d623526f7a
Margarita R <margarita@arrayschool.com>
2018-01-12 14:57:33 -0700

    completed styling on all pages

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,7 +27,7 @@ Rails.application.configure do
   end
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
 
@@ -54,6 +54,14 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.smtp_settings = {
+  #   :user_name => '044f1d81c139b1',
+  #   :password => '292ea176215b5d',
+  #   :address => 'smtp.mailtrap.io',
+  #   :domain => 'smtp.mailtrap.io',
+  #   :port => '2525',
+  #   :authentication => :cram_md5
+  # }
   config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025 }
-  
+
 end
