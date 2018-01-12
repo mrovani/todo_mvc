1a1b6d3e3b12deb967842d92a5363eba8b06226b
Margarita R <margarita@arrayschool.com>
2018-01-12 16:56:22 -0700

    google webfonts fix - take 6

--- config/application.rb
+++ config/application.rb
@@ -29,5 +29,6 @@ module TodoMvc
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")
   end
 end
